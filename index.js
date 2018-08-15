36d3e05da00bf34e4f62547e1a181f13841a1285
iamsostokes <iamsostokes@gmail.com>
2018-08-15 10:15:39 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// document.getElementById("MainPanel").onclick = function() {hideFunction()};
+
+// function hideFunction() { 
+//     var x = document.getElementById("SubPanel");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+    
+// }
+
+
+// $('#MainPanel').click(function(){
+//     $('#SubPanel').toggle('drop');
+    
+   
+// })
+
+
+
+// document.getElementById("MainPanel2").onclick = function() {hideFunction2()};
+
+// function hideFunction2() {
+//     var x = document.getElementById("SubPanel2");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display= "none";
+//     }
+// }
+
+$('#MainPanel2').click(function(){
+    $('#SubPanel2').slideToggle();
+    
+   
+})
+
+// document.getElementById("MainPanel3").onclick = function() {hideFunction3()};
+
+// function hideFunction3() {
+//     var x = document.getElementById("SubPanel3");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display= "none";
+//     }
+// }
+
+$('#MainPanel3').click(function(){
+    $('#SubPanel3').slideToggle();
+    
+   
+})
+
+
+// document.getElementById("MainPanel4").onclick = function() {hideFunction4()};
+
+// function hideFunction4() {
+//     var x = document.getElementById("SubPanel4");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display= "none";
+//     }
+// }
+
+$('#MainPanel4').click(function(){
+    $('#SubPanel4').slideToggle();
+    
+   
+})
